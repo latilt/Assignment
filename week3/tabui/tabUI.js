2a5b3e816cd4c8759da449b86f931fb8c2676efd
HoKyeong Eom <latilt2@gmail.com>
2017-01-23 15:19:59 +0900

    code review2

--- week3/tabui/tabUI.js
+++ week3/tabui/tabUI.js
@@ -20,7 +20,8 @@ var tabUi = {
   },
 
   swapClass : function(swapObj, targetClass) {
-    swapObj.parentElement.querySelector("."+targetClass).classList.remove(targetClass);
+    var preObj = swapObj.parentElement.querySelector("."+targetClass);
+    preObj.classList.remove(targetClass);
     swapObj.classList.add(targetClass);
   },
 
@@ -42,24 +43,6 @@ var tabUi = {
     }
   },
 
-  /*clickTab : function(evt) {
-    var clickedNode = tabUi.clickedNodeCheck(evt.target);
-
-    tabUi.swapClass(clickedNode, tabUi.SWAP_LIST.nav);
-
-    var url = clickedNode.id;
-    var sectionNode = document.querySelector("#my_" + url);
-
-    tabUi.swapClass(sectionNode, tabUi.SWAP_LIST.section);
-
-    if(!!!(sectionNode.children.length)) {
-      var ajax = new XMLHttpRequest();
-      ajax.addEventListener("load", tabUi.ajaxCall);
-      ajax.open("GET", tabUi.URL_LIST[url]);
-      ajax.send();
-    }
-  },*/
-
   ajaxCall : function(res) {
     var resObj = JSON.parse(res.target.responseText);
     var displayNode = document.querySelector(".eleDisplayShow");
@@ -72,57 +55,3 @@ var tabUi = {
     selectedTab.addEventListener("click", this.clickTab.bind(tabUi));
   }
 };
-
-/*
-var URL_LIST = {
-  "position" : "http://jsonplaceholder.typicode.com/posts/1",
-  "friend" : "http://jsonplaceholder.typicode.com/posts/2",
-  "theme" : "http://jsonplaceholder.typicode.com/posts/3",
-  "news" : "http://jsonplaceholder.typicode.com/posts/4"
-};
-
-var SWAP_LIST = {
-  "nav" : "selectedTab",
-  "section" : "eleDisplayShow"
-};
-
-function clickedNodeCheck(nodeObj) {
-  if(nodeObj.classList.contains("tab")) {
-    return nodeObj;
-  } else {
-    return clickedNodeCheck(nodeObj.parentElement);
-  }
-}
-
-function swapClass(swapObj, targetClass) {
-  swapObj.parentElement.querySelector("."+targetClass).classList.remove(targetClass);
-  swapObj.classList.add(targetClass);
-}
-
-var selectedTab = document.querySelector("nav");
-selectedTab.addEventListener("click", function(evt) {
-
-  var clickedNode = clickedNodeCheck(evt.target);
-
-  swapClass(clickedNode, SWAP_LIST.nav);
-
-  var url = clickedNode.id;
-  var sectionNode = document.querySelector("#my_" + url);
-
-  swapClass(sectionNode, SWAP_LIST.section);
-
-  if(!!!(sectionNode.children.length)) {
-    ajax.open("GET", URL_LIST[url]);
-    ajax.send();
-  }
-});
-
-var ajax = new XMLHttpRequest();
-ajax.addEventListener("load", function(res) {
-
-  var resObj = JSON.parse(res.target.responseText);
-  var displayNode = document.querySelector(".eleDisplayShow");
-
-  displayNode.innerHTML = "<ul><li><div class='myName'>" + resObj.title + "</div><div class='myDesc'>" + resObj.body + "</div></li></ul>";
-});
-*/
